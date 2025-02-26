0b12e441b2e27e4eb6b0a226a1d240636fb147ee
강정수 <emdtod12@naver.com>
2025-02-25 18:07:41 -0800

    pageresponse로 return값 변경

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,17 +4,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Post;
+import com.example.demo.dto.PostResponse;
 import com.example.demo.repository.PostRepository;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
 	private final PostRepository postRepository;
 
-	public Page<Post> findPosts(Pageable pageable) {
-		return postRepository.findPostByPageableExecutionUtils(pageable);
+	public Page<PostResponse> findPosts(Pageable pageable) {
+		return postRepository.findPostByPageableExecutionUtils(pageable)
+			.map(PostResponse::of);
 	}
 }
